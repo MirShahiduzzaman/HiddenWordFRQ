f2c8e94e391d574616887ea2c45de9d1b8c66f7d
Mir Shahiduzzaman <mshahiduzzaman8068@bths.edu>
2018-12-14 09:30:03 -0500

    Finished HiddenWord Class
    Signed-off-by: Mir Shahiduzzaman <mshahiduzzaman8068@bths.edu>

--- src/HiddenWord.java
+++ src/HiddenWord.java
@@ -1,2 +1,37 @@
 public class HiddenWord {
+    private String hidden;
+
+    public HiddenWord(String hidden)
+    {
+        this.hidden = hidden;
+    }
+
+    public String getHint(String guess)
+    {
+        String result = "";
+        String check = "";
+
+        for(int i = 0;i<guess.length();i++)
+        {
+            check = guess.substring(i,i+1);
+
+            if(!hidden.contains(check))
+            {
+                result += "*";
+            }
+            else
+            {
+                if(check.equals(hidden.substring(i,i+1)))
+                {
+                    result += check;
+                }
+                else
+                {
+                    result += "+";
+                }
+            }
+        }
+        return result;
+    }
+
 }
