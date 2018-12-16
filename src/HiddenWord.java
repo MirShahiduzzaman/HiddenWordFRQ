85d457a2e259d3dbd996c0dd670397d7cb26484c
Mir Shahiduzzaman <mshahiduzzaman8068@bths.edu>
2018-12-15 19:21:45 -0500

    Finished and Commented.
    Signed-off-by: Mir Shahiduzzaman <mshahiduzzaman8068@bths.edu>

--- src/HiddenWord.java
+++ src/HiddenWord.java
@@ -1,11 +1,23 @@
 public class HiddenWord {
     private String hidden;
 
+    /**
+     * Constructor for HiddenWord Object
+     * @param hidden String to set field hidden to
+     */
     public HiddenWord(String hidden)
     {
         this.hidden = hidden;
     }
 
+    /**
+     * Gives the user a hint as to what the word is based on their input
+     * @param guess guess that the player has made
+     * @return hint with:
+     * '*' if not in the word
+     * '+' if in the word but at another position
+     * the letter if it is in the word and at the correct position
+     */
     public String getHint(String guess)
     {
         String result = "";
